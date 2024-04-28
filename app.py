a0e7ec23f3191803b662d718b9cd0f1df6379181
Mona Lisa <iamdheeraj2614@gmail.com>
2024-04-28 08:06:30 +0530

    flask

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, jsonify, request
 import joblib
 import os
 
@@ -10,19 +10,29 @@ yesorno_model_path = os.path.join("models", "yesorno.pkl")
 safety_model = joblib.load(safety_model_path)
 yesorno_model = joblib.load(yesorno_model_path)
 
+# Define global variables for nutrients
+carbs = None
+fat = None
+fiber = None
+proteins = None
+salts = None
+sugars = None
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/submit', methods=['POST'])
 def submit():
+    global carbs, fat, fiber, proteins, salts, sugars  # Declare variables as global
+
     height = request.form['height']
     weight = request.form['weight']
     diseases = request.form.getlist('diseases')
 
     fat = float(request.form['fat'])
     fiber = float(request.form['fiber'])
-    carbs = float(request.form['carbs'])
+    carbs = float(request.form['carbs'])  # Assign value to global variable
     energy = float(request.form['energy'])
     proteins = float(request.form['proteins'])
     salts = float(request.form['salts'])
@@ -30,20 +40,12 @@ def submit():
 
     body_type = request.form['body_type']
     new = []
-    print("Height:", height)
-    print("Weight:", weight)
-    print("Selected Diseases:", diseases)
 
     nutritional_values = [fat, fiber, carbs, energy, proteins, salts, sugars]
-    print("Nutritional Values:", nutritional_values)
-
-    print("Body Type:", body_type)
-
+    print(nutritional_values)
     safety_score = safety_model.predict([nutritional_values])
 
     yes_or_no = yesorno_model.predict([nutritional_values])
-    print(yes_or_no)
-    print(type(yes_or_no))
 
     if body_type == 'skinny':
         new.append(round(yes_or_no[0][3]))
@@ -60,6 +62,21 @@ def submit():
         new.append(round(yes_or_no[0][2]))
 
     return render_template('index.html', safety_score=safety_score, new=new)
+
+@app.route('/data')
+def get_data():
+    global carbs, fat, fiber, proteins, salts, sugars  # Access global variables
+
+    if None in (carbs, fat, fiber, proteins, salts, sugars):
+        # Return empty data if any nutrient is missing
+        return jsonify({"labels": [], "values": []})
+
+    data = {
+        "labels": ['Fat', 'Fiber', 'Carbs', 'Proteins', 'Salts', 'Sugars'],
+        "values": [fat, fiber, carbs, proteins, salts, sugars]
+    }
+    return jsonify(data)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
